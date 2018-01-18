58be51277ffc2e7228a931482e656715cbf6a962
Tyrel Craig <craig.tyrel@gmail.com>
2018-01-18 09:43:25 -0800

    Brought in KittyInterface, couldn't complete function. Potential problem with multiple return values/truffle parser.

--- contracts/ShipPirates.sol
+++ contracts/ShipPirates.sol
@@ -4,6 +4,24 @@ import "./ShipYard.sol";
 
 contract ShipPirates is ShipYard {
 
+  address ckAddress = 0x06012c8cf97BEaD5deAe237070F9587f8E7A266d;
+  KittyInterface kittyContract = KittyInterface(ckAddress);
+
+//Take in a stray Kitty. If ready Gain a ship with a shield.
+/* 
+Commenting out this function for the moment. Seems to be a break/error with the multiple return values.
+Could be something to do with the Truffle Parser.
+
+function strayKitty(uint _shipId, uint _kittyId) public {
+     bool ready;
+     (,ready,,,,,,,,) = kittyContract.getKitty(_kittyId);
+     require(msg.sender == shipToOwner[_shipId]);
+     Ship storage myShip = armada[_shipId];
+
+ }
+*/
+
+// Take over a new ship and add it to your armada. 
  function pirateNewShip(uint _shipId, uint _targetId) public {
      require(msg.sender == shipToOwner[_shipId]);
      Ship storage myShip = armada[_shipId];
@@ -12,7 +30,8 @@ contract ShipPirates is ShipYard {
      string memory name = strConcat(myShipName, number);
      _createShip(name, _targetId, 1, false);
  }
-
+ 
+ // Helper Function: Convert Unsigned Integer to String. Return a String.
  function uint2str(uint num) internal pure returns (string) {
     if (num == 0) {
          return "0";
@@ -35,6 +54,7 @@ contract ShipPirates is ShipYard {
     return string(bstr);
 }
 
+ //Helper Function: Concatenate two strings. Return a String
  function strConcat(string _name, string _count) internal pure returns (string) {
      bytes memory _nameBytes = bytes(_name);
      bytes memory _countBytes = bytes(_count);
@@ -52,4 +72,20 @@ contract ShipPirates is ShipYard {
     return string(resultBytes);
  }
 
+}
+
+ // Pulling in and working with CryptoKitties Interface.
+contract KittyInterface {
+  function getKitty(uint256 _id) external view returns (
+    bool isGestating,
+    bool isReady,
+    uint256 cooldownIndex,
+    uint256 nextActionAt,
+    uint256 siringWithId,
+    uint256 birthTime,
+    uint256 matronId,
+    uint256 sireId,
+    uint256 generation,
+    uint256 genes
+  );
 }
